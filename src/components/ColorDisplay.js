4124610b3320cf3045e71d650ab9e736ac7af306
mcnadams <mcneilbo@oregonstate.edu>
2019-05-14 14:17:10 -0700

    doin stuff but not really sure why I'm doin it

--- src/components/ColorDisplay.js
+++ src/components/ColorDisplay.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Color.css';
+
+function ColorDisplay({ color }) {
+  return (
+    <div id={styles['color-display']} className={styles[color]}></div>
+  );
+}
+
+ColorDisplay.propTypes = {
+  color: PropTypes.string.isRequired
+};
+
+export default ColorDisplay;
