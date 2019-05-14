f39862882644339824f787b5e9ff965a9afa95fb
mcnadams <mcneilbo@oregonstate.edu>
2019-05-14 13:31:10 -0700

    progress

---
+++ src/components/Incrementer.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react';
+
+export default class Incrementer extends PureComponent {
+  state = {
+    count: 0
+  }
+
+  incrementCounter = () => {
+    this.setState()
+  }
+
+}
