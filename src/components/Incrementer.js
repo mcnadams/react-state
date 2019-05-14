241786a5bb5980fc072086342f9009d7b98135c4
mcnadams <mcneilbo@oregonstate.edu>
2019-05-14 13:39:51 -0700

    made the incrementer

--- src/components/Incrementer.js
+++ src/components/Incrementer.js
@@ -3,10 +3,21 @@ import React, { PureComponent } from 'react';
 export default class Incrementer extends PureComponent {
   state = {
     count: 0
-  }
+  };
 
   incrementCounter = () => {
-    this.setState()
+    this.setState(state => {
+      return { count: state.count + 1 };
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div>Current Count: { this.state.count }</div>
+        <button onClick={this.incrementCounter}>Increment</button>
+      </>
+    );
   }
 
 }
