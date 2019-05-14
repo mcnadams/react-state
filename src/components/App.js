59b4fb8eeac7ebc0c4e16e3b4645ea6a914eec51
mcnadams <mcneilbo@oregonstate.edu>
2019-05-14 13:51:10 -0700

    stuff

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorPicker from './ColorPicker';
+import ColorDisplay from './ColorDisplay';
+
+export default function App() {
+  return (
+    <>
+      <ColorPicker />
+      <ColorDisplay />
+    </>
+  );
+}
